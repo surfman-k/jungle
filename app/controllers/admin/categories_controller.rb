8836e56d85e02d670975b122efcadaec6f9bdf75
Konrad <konrad.kipczak@gmail.com>
2018-03-01 19:09:02 +0000

    added basic authentication for admin access

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,4 +1,6 @@
 class Admin::CategoriesController < ApplicationController
+  http_basic_authenticate_with name: ENV["ADMIN_NAME"], password: ENV["ADMIN_PASS"]
+
 	 def index
     @categories = Category.order(id: :desc).all
   end
