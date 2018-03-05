fa9d09e3bdd78e7fa323ffabd98ab2ae06da8e32
Konrad <konrad.kipczak@gmail.com>
2018-03-05 18:43:27 +0000

    added more tests for user registration

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ActiveRecord::Base
 	has_secure_password
 	has_many :reviews
+
+	validates :name, presence: true
+  validates :email, presence: true
+  validates :password_digest, length: {minimum: 6}
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,8 +4,33 @@ RSpec.describe User, type: :model do
   describe 'Validations' do
 
   	it 'should add to db if user gives valid credentials' do
-  		@user = User.new({name: 'Johnnyboy', email: "johnnyboy@gmail.com", password_digest: "passman"})
+  		@user = User.new({name: 'Johnnyboy', email: "johnnyboy@gmail.com", password_digest: "passwprd"})
   		@user.save!
+  	end
+
+  	it 'should throw error if user fails to give name' do
+  		@user = User.new({email: "johnnyboy@gmail.com", password_digest: "passman"})
+  		expect {  @user.save! }.to raise_error(ActiveRecord::RecordInvalid)
+  	end
+
+  	it 'should throw error if user fails to give matching passwords' do
+  		@user = User.new({name: 'Johnnyboy', email: "johnnyboy@gmail.com"})
+  		expect {  @user.save! }.to raise_error(ActiveRecord::RecordInvalid)
+  	end
+
+  	it 'should throw error if user fails to give email' do
+  		@user = User.new({name: 'Johnnyboy', password_digest: "passman"})
+  		expect {  @user.save! }.to raise_error(ActiveRecord::RecordInvalid)
+  	end
+
+  	it 'should throw error if user fails to give email' do
+  		@user = User.new({name: 'Johnnyboy', password_digest: "passman"})
+  		expect {  @user.save! }.to raise_error(ActiveRecord::RecordInvalid)
+  	end
+
+  	it 'should throw error if user gives too short of a password' do
+  		@user = User.new({name: 'Johnnyboy', email: "johnnyboy@gmail.com", password_digest: "pass"})
+  		expect {  @user.save! }.to raise_error(ActiveRecord::RecordInvalid)
   	end
 
   end
